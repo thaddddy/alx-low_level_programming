c6303fefcd2ca39f891745620c316db826d7457b
Thaddy <oyeleyetobiloba731@gmail.com>
2023-08-22 13:35:07 -0700

    Work

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -24,9 +24,9 @@ int main(void)
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 
-		if ((2272 - sum) - '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 2272 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
