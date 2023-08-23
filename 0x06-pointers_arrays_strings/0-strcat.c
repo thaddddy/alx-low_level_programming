09e25f748cbea1986de42de739f10456fe5b7307
Thaddy <oyeleyetobiloba731@gmail.com>
2023-08-23 05:53:28 -0700

    Work

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int length_of_string, z;
+
+length_of_string = 0;
+
+while (dest[length_of_string] != '\0')
+{
+length_of_string++;
+}
+
+for (z = 0; src[z] != '\0'; z++, length_of_string++)
+{
+dest[length_of_string] = src[z];
+}
+dest[length_of_string] = '\0';
+return (dest);
+
+}
