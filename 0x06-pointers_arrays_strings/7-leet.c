ce16b01df4f6255cdb98419e2386f497714b7108
Thaddy <oyeleyetobiloba731@gmail.com>
2023-08-23 14:46:50 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * Letters a and A are replaced by 4.
+ * Letters e and E are replaced by 3.
+ * letters o and O are replaced by 0.
+ * Letters t and T are replaced by 7.
+ * Letters l and L are replaced by 1.
+ * @s: pointer to string
+ *
+ * Return: pointer to s.
+ */
+
+
+char *leet(char *s)
+{
+	int string_length, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+/* scan through string */
+	string_length = 0;
+	while (leetCount < 10)
+	{
+		if (leetLetters[leetCount] == s[string_length])
+		{
+			s[string_length] = leetNums[leetCount];
+		}
+		string_length++;
+	}
+	return (s);
+
+}
