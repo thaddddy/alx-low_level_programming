b980dad9744e09629807f2a09c82d9e28ffc00fa
Thaddy <oyeleyetobiloba731@gmail.com>
2023-09-14 17:44:49 -0700

    work:

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,13 +8,12 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i, check_stat;
+	int i, check_stat; /* declare variables and va_arg datatype */
 
 	char *str;
 	va_list spc;
 
-	va_start(spc, format);
-
+	va_start(spc, format); /* initialize var argguements */
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
@@ -22,7 +21,7 @@ void print_all(const char * const format, ...)
 		{
 			case 'i':
 				printf("%d", va_arg(spc, int));
-				check_stat = 0;
+				check_stat = 0; /* check if stat has been met */
 				break;
 			case 'f':
 				printf("%f", va_arg(spc, double));
@@ -35,17 +34,17 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(spc, char *);
 				if (str == NULL)
-					str = "(nill)";
+					str = "(nil)";
 				printf("%s", str);
 				break;
 			default:
 				check_stat = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && check_stat == 0)
+		if (format[i + 1] != '\0' && check_stat == 0) /* if NOT NULL */
 			printf(", ");
-		i++;
+		i++; /* update step of iter var */
 	}
 	printf("\n");
-	va_end(spc);
+	va_end(spc); /* end traversal */
 }
