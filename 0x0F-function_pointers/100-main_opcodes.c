81d1fcdeabf83afc57c73d80570999f78c8dd582
Thaddy <oyeleyetobiloba731@gmail.com>
2023-09-14 19:34:16 -0700

    Fucntions work

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_opcodes - print the opcodes of the program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ * Return: void
+ */
+
+void print_opcodes(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%.2hhx", a[i]);
+	if (i < n - 1)
+		printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - print the opcodes of itws own main function
+ * @argc: arguement count
+ * @argv: narguement vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	if (argc != 2)
+	{
+	printf("Error\n");
+	exit(1);
+	}
+
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	print_opcodes((char *)&main, n);
+	return (0);
+}
